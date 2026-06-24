95e1b67ecad6d3651b32775f49dc38b69e228dd9
Hongfei <ghf1127945424@163.com>
2026-06-24 18:44:56 +0800

    Initial Commit

---
+++ PokemonDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PokemonDemo
+//
+//  Created by 顾鸿飞 on 2026/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PokemonDemo/PokemonDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonDemoApp.swift
+//  PokemonDemo
+//
+//  Created by 顾鸿飞 on 2026/6/24.
+//
+
+import SwiftUI
+
+@main
+struct PokemonDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
